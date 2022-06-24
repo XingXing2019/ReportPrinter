b61609a79a12e417d64de4cb0c8261b33200ddca
Xing <x.xing.unsw@gmail.com>
2022-06-24 15:23:17 +1000

    init

---
+++ ReportPrinter/MalachiService/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using RaphaelService;
+
+namespace MalachiService
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = new Class1();
+        }
+    }
+}
---
+++ ReportPrinter/RaphaelService/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace RaphaelService
+{
+    public class Class1
+    {
+    }
+}

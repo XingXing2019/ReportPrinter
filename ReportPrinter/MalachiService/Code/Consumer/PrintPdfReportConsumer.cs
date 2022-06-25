a89dab14d5a9bb223bd9d4c12cc06c9087ea2026
Xing <x.xing.unsw@gmail.com>
2022-06-25 23:20:52 +1000

    Log message published and received

--- ReportPrinter/MalachiService/Code/Consumer/PrintPdfReportConsumer.cs
+++ ReportPrinter/MalachiService/Code/Consumer/PrintPdfReportConsumer.cs
@@ -1,6 +1,7 @@
 ﻿using System.Threading.Tasks;
 using MassTransit;
 using RaphaelService;
+using ReportPrinterLibrary.Log;
 using ReportPrinterLibrary.RabbitMQ.Message;
 
 namespace MalachiService.Code.Consumer
@@ -9,7 +10,10 @@ namespace MalachiService.Code.Consumer
     {
         public async Task Consume(ConsumeContext<IPrintPdfReport> context)
         {
+            var procName = $"{this.GetType().Name}.{nameof(Consume)}";
             var msg = context.Message;
+            Logger.LogJson($"{nameof(PrintLabelReportConsumer)} receive message", msg, procName);
+
             var a = new Class1();
         }
     }

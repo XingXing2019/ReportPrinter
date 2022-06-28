f0aca125d3f4936f8d6bc4e00595660816ca779c
Xing <x.xing.unsw@gmail.com>
2022-06-28 20:18:00 +1000

    Improve UI to send message

---
+++ ReportPrinter/CosmoService/Code/Form/frmPublishPrintReport.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Windows.Forms;
+using CosmoService.Code.Producer.PrintReportCommand;
+using ReportPrinterDatabase.Manager.MessageManager.PrintReportMessage;
+using ReportPrinterLibrary.RabbitMQ.Message;
+using ReportPrinterLibrary.RabbitMQ.Message.PrintReportMessage;
+using ReportPrinterLibrary.RabbitMQ.MessageQueue;
+
+namespace CosmoService.Code.Form
+{
+    public partial class frmPublishPrintReport : System.Windows.Forms.Form
+    {
+        public frmPublishPrintReport()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnSend_Click(object sender, EventArgs e)
+        {
+            var queueName = rdbPDF.Checked ? QueueName.PDF_QUEUE : QueueName.LABEL_QUEUE;
+            var producer = PrintReportProducerFactory.CreatePrintReportProducer(queueName, new PrintReportMessageManager());
+            
+            var hasError = false;
+            if (rdbPDF.Checked)
+            {
+                if (!MessageDeserializer<PrintPdfReport>.Deserialize(txtMessage.Text, out var message) || !message.IsValid)
+                    hasError = true;
+                else
+                    await producer.ProduceAsync(message);
+            }
+            else if (rdbLabel.Checked)
+            {
+                if (!MessageDeserializer<PrintLabelReport>.Deserialize(txtMessage.Text, out var message) || !message.IsValid)
+                    hasError = true;
+                else
+                    await producer.ProduceAsync(message);
+            }
+
+            if (hasError)
+            {
+                var error = $"There is an error in the input message text, stop sending";
+                MessageBox.Show(error);
+            }
+        }
+    }
+}

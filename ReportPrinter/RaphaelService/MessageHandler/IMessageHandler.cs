391fc8aa604a05a7b7f3eefde7d697028346f743
Xing <x.xing.unsw@gmail.com>
2022-06-28 11:42:10 +1000

    Create message handler

---
+++ ReportPrinter/RaphaelService/MessageHandler/IMessageHandler.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using ReportPrinterLibrary.RabbitMQ.Message;
+
+namespace RaphaelService.MessageHandler
+{
+    public interface IMessageHandler
+    {
+        Task Handle(IMessage message);
+    }
+}
---
+++ ReportPrinter/RaphaelService/MessageHandler/PrintReportMessageHandler/PrintPdfReportMessageHandler.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+using ReportPrinterLibrary.RabbitMQ.Message;
+
+namespace RaphaelService.MessageHandler.PrintReportMessageHandler
+{
+    public class PrintPdfReportMessageHandler : IMessageHandler
+    {
+        public async Task Handle(IMessage message)
+        {
+            //throw new System.NotImplementedException();
+        }
+    }
+}

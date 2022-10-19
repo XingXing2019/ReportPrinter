f41c0822d49b9499895187fd745c296c7f90e89f
Xing <x.xing.unsw@gmail.com>
2022-10-19 20:22:00 +1100

    Test label structure

--- ReportPrinter/ReportPrinterUnitTest/TestBase.cs
+++ ReportPrinter/ReportPrinterUnitTest/TestBase.cs
@@ -32,8 +32,7 @@ namespace ReportPrinterUnitTest
 
             _random = new Random();
         }
-
-        #region Helper
+        
 
         protected IPrintReport CreateMessage(ReportTypeEnum reportType, bool isValidMessage = true)
         {
@@ -84,110 +83,7 @@ namespace ReportPrinterUnitTest
                 Assert.IsTrue(actual.SqlVariables.Any(x => x.Name == variable.Name && x.Value == variable.Value));
             }
         }
-
-        protected void AssertObject(object obj1, object obj2)
-        {
-            try
-            {
-                Assert.AreEqual(obj1.GetType(), obj2.GetType());
-                var type = obj1.GetType();
-
-                if (!type.IsClass || type == typeof(string))
-                    Assert.AreEqual(obj1, obj2);
-                else
-                {
-                    var propInfos = type.GetProperties(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
-                    foreach (var prop in propInfos)
-                    {
-                        var value1 = prop.GetValue(obj1);
-                        var value2 = prop.GetValue(obj2);
-
-                        if (value1 == null && value2 == null)
-                            continue;
-                        if (value1 == null || value2 == null)
-                            throw new ApplicationException($"Could not get value of obj1 or obj2");
-
-                        if (prop.PropertyType.IsInterface)
-                            AssertObject(value1, value2);
-                        else if (!prop.PropertyType.IsClass || prop.PropertyType == typeof(string))
-                            Assert.AreEqual(value1, value2);
-                        else if (!typeof(IEnumerable).IsAssignableFrom(prop.PropertyType))
-                            AssertObject(value1, value2);
-                        else if (prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(Dictionary<,>))
-                            AssertDictionary(value1, value2);
-                        else if (typeof(IEnumerable).IsAssignableFrom(prop.PropertyType))
-                            AssertList(prop.PropertyType, value1, value2);
-                        else
-                            throw new ApplicationException($"Unknown field type");
-                    }
-
-                    var fieldInfos = type.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance).Where(x => !x.Name.Contains("k__BackingField")).ToArray();
-                    foreach (var field in fieldInfos)
-                    {
-                        var value1 = field.GetValue(obj1);
-                        var value2 = field.GetValue(obj2);
-
-                        if (value1 == null && value2 == null)
-                            continue;
-                        if (value1 == null || value2 == null)
-                            throw new ApplicationException($"Could not get value of obj1 or obj2");
-
-                        if (field.FieldType.IsInterface)
-                            AssertObject(value1, value2);
-                        else if (!field.FieldType.IsClass || field.FieldType == typeof(string))
-                            Assert.AreEqual(value1, value2);
-                        else if (!typeof(IEnumerable).IsAssignableFrom(field.FieldType))
-                            AssertObject(value1, value2);
-                        else if (field.FieldType.IsGenericType && field.FieldType.GetGenericTypeDefinition() == typeof(Dictionary<,>))
-                            AssertDictionary(value1, value2);
-                        else if (typeof(IEnumerable).IsAssignableFrom(field.FieldType))
-                            AssertList(field.FieldType, value1, value2);
-                        else
-                            throw new ApplicationException($"Unknown field type");
-                    }
-                }
-            }
-            catch (Exception ex)
-            {
-                Assert.Fail(ex.Message);
-            }
-        }
-
-        private void AssertDictionary(object value1, object value2)
-        {
-            var dict1 = (IDictionary)value1;
-            var dict2 = (IDictionary)value2;
-
-            Assert.AreEqual(dict1.Count, dict2.Count);
-            foreach (var key in dict1.Keys)
-            {
-                Assert.IsTrue(dict2.Contains(key));
-                AssertObject(dict1[key], dict2[key]);
-            }
-        }
-
-        private void AssertList(Type type, object value1, object value2)
-        {
-            var itemType = type.GetElementType() ?? type.GenericTypeArguments[0];
-            var listType = typeof(List<>).MakeGenericType(itemType);
-            var list1 = (IList)Activator.CreateInstance(listType);
-            var list2 = (IList)Activator.CreateInstance(listType);
-
-            if (list1 == null || list2 == null)
-            {
-                throw new ApplicationException($"List of {itemType.Name} is not created");
-            }
-
-            foreach (var item in (IEnumerable)value1)
-                list1.Add(item);
-            foreach (var item in (IEnumerable)value2)
-                list2.Add(item);
-
-            Assert.AreEqual(list1.Count, list2.Count);
-            for (int i = 0; i < list1.Count; i++)
-                AssertObject(list1[i], list2[i]);
-        }
-
+        
         protected T GetPrivateField<T>(Type objectType, string fieldName, object instance)
         {
             var fieldInfo = objectType.GetField(fieldName, BindingFlags.NonPublic | BindingFlags.Instance);
@@ -249,6 +145,112 @@ namespace ReportPrinterUnitTest
 
                 labelStructureList.Add(labelStructureId, labelStructure);
             }
+        }
+
+        protected void AssertObject(object obj1, object obj2)
+        {
+            try
+            {
+                Assert.AreEqual(obj1.GetType(), obj2.GetType());
+                var type = obj1.GetType();
+
+                if (!type.IsClass || type == typeof(string))
+                    Assert.AreEqual(obj1, obj2);
+                else
+                {
+                    var propInfos = type.GetProperties(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance);
+                    foreach (var prop in propInfos)
+                    {
+                        var value1 = prop.GetValue(obj1);
+                        var value2 = prop.GetValue(obj2);
+
+                        if (value1 == null && value2 == null)
+                            continue;
+                        if (value1 == null || value2 == null)
+                            throw new ApplicationException($"Could not get value of obj1 or obj2");
+
+                        if (prop.PropertyType.IsInterface)
+                            AssertObject(value1, value2);
+                        else if (!prop.PropertyType.IsClass || prop.PropertyType == typeof(string))
+                            Assert.AreEqual(value1, value2);
+                        else if (prop.PropertyType.IsGenericType && prop.PropertyType.GetGenericTypeDefinition() == typeof(Dictionary<,>))
+                            AssertDictionary(value1, value2);
+                        else if (typeof(IEnumerable).IsAssignableFrom(prop.PropertyType))
+                            AssertList(prop.PropertyType, value1, value2);
+                        else if (prop.PropertyType.IsClass)
+                            AssertObject(value1, value2);
+                        else
+                            throw new ApplicationException($"Unknown field type");
+                    }
+
+                    var fieldInfos = type.GetFields(BindingFlags.NonPublic | BindingFlags.Public | BindingFlags.Instance).Where(x => !x.Name.Contains("k__BackingField")).ToArray();
+                    foreach (var field in fieldInfos)
+                    {
+                        var value1 = field.GetValue(obj1);
+                        var value2 = field.GetValue(obj2);
+
+                        if (value1 == null && value2 == null)
+                            continue;
+                        if (value1 == null || value2 == null)
+                            throw new ApplicationException($"Could not get value of obj1 or obj2");
+
+                        if (field.FieldType.IsInterface)
+                            AssertObject(value1, value2);
+                        else if (!field.FieldType.IsClass || field.FieldType == typeof(string))
+                            Assert.AreEqual(value1, value2);
+                        else if (field.FieldType.IsGenericType && field.FieldType.GetGenericTypeDefinition() == typeof(Dictionary<,>))
+                            AssertDictionary(value1, value2);
+                        else if (typeof(IEnumerable).IsAssignableFrom(field.FieldType))
+                            AssertList(field.FieldType, value1, value2);
+                        else if (field.FieldType.IsClass)
+                            AssertObject(value1, value2);
+                        else
+                            throw new ApplicationException($"Unknown field type");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+
+
+        #region Helper
+
+        private void AssertDictionary(object value1, object value2)
+        {
+            var dict1 = (IDictionary)value1;
+            var dict2 = (IDictionary)value2;
+
+            Assert.AreEqual(dict1.Count, dict2.Count);
+            foreach (var key in dict1.Keys)
+            {
+                Assert.IsTrue(dict2.Contains(key));
+                AssertObject(dict1[key], dict2[key]);
+            }
+        }
+
+        private void AssertList(Type type, object value1, object value2)
+        {
+            var itemType = type.GetElementType() ?? type.GenericTypeArguments[0];
+            var listType = typeof(List<>).MakeGenericType(itemType);
+            var list1 = (IList)Activator.CreateInstance(listType);
+            var list2 = (IList)Activator.CreateInstance(listType);
+
+            if (list1 == null || list2 == null)
+            {
+                throw new ApplicationException($"List of {itemType.Name} is not created");
+            }
+
+            foreach (var item in (IEnumerable)value1)
+                list1.Add(item);
+            foreach (var item in (IEnumerable)value2)
+                list2.Add(item);
+
+            Assert.AreEqual(list1.Count, list2.Count);
+            for (int i = 0; i < list1.Count; i++)
+                AssertObject(list1[i], list2[i]);
         }
 
         #endregion

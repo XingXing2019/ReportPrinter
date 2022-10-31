73053f75c551b6e95e02dec817c3aa99a52092ba
Xing <x.xing.unsw@gmail.com>
2022-10-31 21:45:56 +1100

    Test Factory

---
+++ ReportPrinter/ReportPrinterUnitTest/RaphaelLibrary/Common/SqlVariableCacheManager/SqlVariableCacheManagerFactoryTest.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using RaphaelLibrary.Code.Common.SqlResultCacheManager;
+using RaphaelLibrary.Code.Common.SqlVariableCacheManager;
+using ReportPrinterLibrary.Code.Config.Configuration;
+using System;
+
+namespace ReportPrinterUnitTest.RaphaelLibrary.Common.SqlVariableCacheManager
+{
+    public class SqlVariableCacheManagerFactoryTest
+    {
+        [Test]
+        [TestCase(CacheManagerType.Memory, typeof(SqlVariableMemoryCacheManager))]
+        [TestCase(CacheManagerType.Redis, typeof(SqlVariableRedisCacheManager))]
+        [TestCase(2, null)]
+        public void TestCreateSqlResultCacheManager(CacheManagerType managerType, Type expectedType)
+        {
+            try
+            {
+                var manager = SqlVariableCacheManagerFactory.CreateSqlVariableCacheManager(managerType);
+                Assert.AreEqual(expectedType, manager.GetType());
+            }
+            catch (InvalidOperationException ex)
+            {
+                var expectedError = $"Invalid type: {managerType} for sql variable cache manager";
+                Assert.AreEqual(expectedError, ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Assert.Fail(ex.Message);
+            }
+        }
+    }
+}

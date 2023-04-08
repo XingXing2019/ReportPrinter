ab2d476ab26543ac1476055ebaf2128b830ecf29
Xing <x.xing.unsw@gmail.com>
2023-04-08 19:06:28 +1000

    Update UpdateDatabase script

--- ReportPrinter/ReportPrinterDatabase/Code/SqlQuery/StoredProcedure/UpdateDatabase.bat
+++ ReportPrinter/ReportPrinterDatabase/Code/SqlQuery/StoredProcedure/UpdateDatabase.bat
@@ -4,9 +4,6 @@ REM ----------------------------------------------------------------------------
 REM  Remove Existing file list and Creating new one based on the SQL files available including sub folders
 REM --------------------------------------------------------------------------------------------------------
 
-ECHO Deploy stored procedures on ReportPrinterTest database. 
-ECHO.
-
 IF EXIST list.txt DEL /Q .\list.txt
 FOR /f %%i in ('DIR *.sql /s /b 2^> nul ^| FIND "" /v /c') do SET FileCount=%%i
 
@@ -20,40 +17,38 @@ IF NOT "%FileCount%"=="0" (
     DIR *.sql /b /s /a-d >> list.txt
 )
 
-
 REM ----------------------------------------------------------------------------------
-REM                 Collecting Server, Database and Credential
+REM                 Set Server and Database
 REM ----------------------------------------------------------------------------------
 
 SET server=LAPTOP-ETHALNIF
-SET catalog=ReportPrinterTest
+SET database=ReportPrinterTest
 SET _sqllist=list.txt
+
+ECHO ======================================== Update %database% database ========================================
+ECHO.
 
 FOR /f "tokens=*" %%f in (%_sqllist%) do (
     ECHO Executing file "%%f"
     FOR %%A in ("%%f") do (
-        sqlcmd -S %server% -d %catalog% -i "%%f"
+        sqlcmd -S %server% -d %database% -i "%%f"
     )
 )
 
 ECHO.
-ECHO Deploy stored procedures on ReportPrinter database. 
+ECHO ======================================== Update %database% database ========================================
 ECHO.
 
-SET catalog=ReportPrinter
+SET database=ReportPrinter
 
 FOR /f "tokens=*" %%f in (%_sqllist%) do (
     ECHO Executing file "%%f"
     FOR %%A in ("%%f") do (
-        sqlcmd -S %server% -d %catalog% -i "%%f"
+        sqlcmd -S %server% -d %database% -i "%%f"
     )
 )
 
 SET _sqllist=
 ECHO.
-ECHO Process completed. Please verify the scrits executed in list.txt file.
-ECHO.
-GOTO commonexit
-
-:commonexit
+ECHO Process completed. Please verify the scripts executed in list.txt file.
 PAUSE
